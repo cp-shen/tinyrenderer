8bc1f9471421fefe19ecd89380cdf89908d5cc63
scp <cp_shen@outlook.com>
2019-09-10 17:34:04 +0800

    formatting

--- model.h
+++ model.h
@@ -1,22 +1,23 @@
 #ifndef __MODEL_H__
 #define __MODEL_H__
-#include <vector>
-#include <string>
 #include "geometry.h"
 #include "tgaimage.h"
+#include <string>
+#include <vector>
 
 class Model {
 private:
     std::vector<Vec3f> verts_;
-    std::vector<std::vector<Vec3i> > faces_; // attention, this Vec3i means vertex/uv/normal
+    std::vector<std::vector<Vec3i>> faces_; // attention, this Vec3i means vertex/uv/normal
     std::vector<Vec3f> norms_;
     std::vector<Vec2f> uv_;
     TGAImage diffusemap_;
     TGAImage normalmap_;
     TGAImage specularmap_;
-    void load_texture(std::string filename, const char *suffix, TGAImage &img);
+    void load_texture(std::string filename, const char* suffix, TGAImage& img);
+
 public:
-    Model(const char *filename);
+    Model(const char* filename);
     ~Model();
     int nverts();
     int nfaces();
@@ -30,4 +31,3 @@ public:
     std::vector<int> face(int idx);
 };
 #endif //__MODEL_H__
-
